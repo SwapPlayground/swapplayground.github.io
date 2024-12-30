f2dd9ea4ad5f99524493910881a79987f914ab25
nicolae-leonte-go <nicolae.leonte.go@gmail.com>
2024-12-30 21:48:50 +0000

    remove dev

--- dev/_next/static/chunks/506.61a77691d9666c91.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[506],{10985:function(){},11314:function(){},81388:function(){}}]);
--- dev/_next/static/chunks/pages/_app-29df7944f885ca50.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{66916:function(_,n,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return e(51207)}])}},function(_){var __webpack_exec__=function(n){return _(_.s=n)};_.O(0,[774,179],function(){return __webpack_exec__(66916),__webpack_exec__(27900)}),_N_E=_.O()}]);
